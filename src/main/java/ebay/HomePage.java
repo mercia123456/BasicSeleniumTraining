1ba2fa8c450d9b77880b9d3d5e95589dc5871ba7
“NaimKhanSiam” <naim_khan@live.co.uk>
2019-05-01 22:37:39 -0400

    selenium methods,screenshot

---
+++ src/main/java/ebay/HomePage.java
@@ -0,0 +1,10 @@
+package ebay;
+
+import initialization.Setup;
+
+public class HomePage extends Setup {
+
+    //Homework :    30 Test Cases From Ebay (Not 1 or 2 clicks, implement all that we have learned so far in different test cases)
+    //Run some test cases in the mozilla,some in the chrome
+    //Take screenshot
+}
